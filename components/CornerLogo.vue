839eed10e8b55d6d5cca434059129af7bb9a0c64
Danilo Velasquez <danilo.velasquez@adevinta.com>
2025-06-03 11:41:32 +0200

    added cornerlogo component

---
+++ components/CornerLogo.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :src="src" :alt="alt" :style="logoStyle" class="corner-logo" />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  src: { type: String, required: true },
+  alt: { type: String, default: '' },
+  height: { type: [String, Number], default: '250px' }
+})
+
+const logoStyle = computed(() => ({
+  height: typeof props.height === 'number' ? props.height + 'px' : props.height
+}))
+</script>
+
+<style scoped>
+.corner-logo {
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+  opacity: 0.9;
+  z-index: 10;
+}
+</style> 
