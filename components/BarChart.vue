1340b8626a68f3cadff3fe9a968680b642aa6051
Danilo Velasquez <danilo.velasquez@adevinta.com>
2025-06-05 18:35:34 +0200

    updated performance wins

---
+++ components/BarChart.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart, BarElement, CategoryScale, LinearScale, LogarithmicScale, Tooltip, Legend } from 'chart.js'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+
+Chart.register(BarElement, CategoryScale, LinearScale, LogarithmicScale, Tooltip, Legend, ChartDataLabels)
+
+const props = defineProps({
+  data: Object,
+  options: Object
+})
+</script>
+
+<template>
+  <Bar :data="data" :options="options" />
+</template> 
